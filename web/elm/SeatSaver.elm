e6a7fcb0058a4e9f5180d8d8575ff827a1861aec
Alan Gardner <urfolomeus@gmail.com>
2015-09-22 22:30:59 +0100

    4. Setting up a basic Elm architecture

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -3,14 +3,74 @@ module SeatSaver where
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+import StartApp exposing (App)
+import Effects exposing (Effects, Never)
+import Task exposing (Task)
 
+
+-- MODEL
+
+type alias Seat =
+  { seatNo : Int
+  , occupied : Bool
+  }
+
+
+type alias Model =
+  List Seat
+
+
+initialModel : (Model, Effects Action)
+initialModel =
+  let
+    seats = [ {seatNo = 1, occupied = True}
+            , {seatNo = 2, occupied = False}
+            ]
+  in
+    (seats, Effects.none)
+
+
+-- UPDATE
+
+type Action = NoOp
+
+
+update : Action -> Model -> (Model, Effects Action)
+update action model =
+  case action of
+    NoOp ->
+      (model, Effects.none)
+
+
+-- VIEW
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  ul [ ] ( List.map (seatItem address) model )
+
+
+seatItem : Signal.Address Action -> Seat -> Html
+seatItem address seat =
+  li [ ] [ text (toString seat) ]
+
+
+-- WIRING
+
+app : App Model
+app =
+  StartApp.start
+    { init = initialModel
+    , update = update
+    , view = view
+    , inputs = []
+    }
+
+
+main : Signal Html
 main =
-  view
+  app.html
 
 
-view =
-  div [ class "jumbotron" ]
-    [ h2 [ ] [ text "Welcome to Elm!" ]
-    , p [ class "lead" ]
-      [ text "The best of functional programming in your browser" ]
-    ]
+port tasks : Signal (Task Never ())
+port tasks =
+  app.tasks
