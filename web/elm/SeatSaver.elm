d3e08305b71930d8af8480b57cd9cc3ce54c9e63
Alan Gardner <urfolomeus@gmail.com>
2016-03-29 17:26:38 +0100

    4. Adding a Model and enhancing the View

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -1,11 +1,44 @@
 module SeatSaver where
 
-import Html
+import Html exposing (ul, li, text)
+import Html.Attributes exposing (class)
 
 main =
-  view
+  view init
+
+
+-- MODEL
+
+type alias Seat =
+  { seatNo : Int
+  , occupied : Bool
+  }
+
+
+type alias Model =
+  List Seat
+
+
+init =
+  [ { seatNo = 1, occupied = False }
+  , { seatNo = 2, occupied = False }
+  , { seatNo = 3, occupied = False }
+  , { seatNo = 4, occupied = False }
+  , { seatNo = 5, occupied = False }
+  , { seatNo = 6, occupied = False }
+  , { seatNo = 7, occupied = False }
+  , { seatNo = 8, occupied = False }
+  , { seatNo = 9, occupied = False }
+  , { seatNo = 10, occupied = False }
+  , { seatNo = 11, occupied = False }
+  , { seatNo = 12, occupied = False }
+  ]
+
 
 -- VIEW
 
-view =
-  Html.text "Woo hoo, I'm in a View"
+view model =
+  ul [ class "seats" ] (List.map seatItem model)
+
+seatItem seat =
+  li [ class "seat available" ] [ text (toString seat.seatNo) ]
