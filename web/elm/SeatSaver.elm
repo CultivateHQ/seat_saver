a880c3ae2697db309fd587e01a500818b716745f
Alan Gardner <urfolomeus@gmail.com>
2016-03-29 18:02:43 +0100

    6. Adding an Update

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -2,11 +2,17 @@ module SeatSaver where
 
 import Html exposing (..)
 import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+import StartApp.Simple
 
 
-main : Html
+main : Signal Html
 main =
-  view init
+  StartApp.Simple.start
+    { model = init
+    , update = update
+    , view = view
+    }
 
 
 -- MODEL
@@ -38,13 +44,38 @@ init =
   ]
 
 
+-- UPDATE
+
+type Action = Toggle Seat
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Toggle seatToToggle ->
+      let
+        updateSeat seatFromModel =
+          if seatFromModel.seatNo == seatToToggle.seatNo then
+            { seatFromModel | occupied = not seatFromModel.occupied }
+          else seatFromModel
+      in
+        List.map updateSeat model
+
+
 -- VIEW
 
-view : Model -> Html
-view model =
-  ul [ class "seats" ] (List.map seatItem model)
+view : Signal.Address Action -> Model -> Html
+view address model =
+  ul [ class "seats" ] (List.map (seatItem address) model)
 
 
-seatItem : Seat -> Html
-seatItem seat =
-  li [ class "seat available" ] [ text (toString seat.seatNo) ]
+seatItem : Signal.Address Action -> Seat -> Html
+seatItem address seat =
+  let
+    occupiedClass =
+      if seat.occupied then "occupied" else "available"
+  in
+    li
+      [ class ("seat " ++ occupiedClass)
+      , onClick address (Toggle seat)
+      ]
+      [ text (toString seat.seatNo) ]
